3d1a5ce977802b3c961b06c4895edf62cd464fe6
Casey Handmer <casey.handmer@gmail.com>
2018-06-30 23:31:43 -0700

    Placed draft algorithm in comments

--- EvolveArray.py
+++ EvolveArray.py
@@ -9,3 +9,49 @@
 
 # 5.5) set new ghost zones to old ghost zones
 # 6) Do it all again for NS.
+
+
+# This script also initializes an array containing ghost zone information, which is how adjacent sectors pass depth information back and forth.
+
+# User set parameters
+res = 45
+
+# Path to memory location for arrays of a particular resolution
+thisdir = "/home/handmer/Documents/Mars/water-flow/"
+
+inputpath = thisdir + "res"+str(res)+"/"
+
+
+import numpy as np
+
+# Create ghost zone arrays. EW, NS, old and new. Contain only depth information.
+
+# Allocate graticule memory
+
+#loop time steps
+
+# refresh ghost zones
+
+# loop over graticules
+
+# load graticule
+test = np.load(inputpath+"test00.npy")
+
+# update ghost zones from old ghost zone
+
+# Compute flow for EW in place
+
+# Update graticule depths
+
+# save graticule
+
+# update ghost zones (new)
+
+# Do the same for NS
+
+
+
+
+
+
+print(test[:,:,1])
