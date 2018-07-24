ec85c2ceea20e5ff8b5006e8e0dd1fb64b22ea8b
Casey Handmer <casey.handmer@gmail.com>
2018-07-23 22:02:49 -0700

    Automated global output

--- PlotArrays2.py
+++ PlotArrays2.py
@@ -5,6 +5,8 @@ zoom=2**4
 res = int(45*zoom)
 print("res = " + str(res))
 
+colors = 1
+
 # Path to memory location for arrays of a particular resolution
 thisdir = "/home/handmer/Documents/Mars/water-flow/"
 
@@ -16,29 +18,79 @@ from matplotlib import image as im
 import pandas as pd
 import colorsys as cs
 
-place = [2,3]
-rawdata = np.load(inputpath+"/array"+str(place[0])+str(place[1])+".npy")[2:-2,2:-2]
+# Functions
 
-mola = rawdata[:,:,0]
-depth = rawdata[:,:,2]
-flowNS2 = np.gradient(mola+depth, axis=0)
-flowNS = rawdata[:,:,3]
-flowEW2 = np.gradient(mola+depth, axis=1)
-flowEW = rawdata[:,:,4]
-flownorm = rawdata[:,:,7]
+#Time to home cook a HSB2RGB function.
+# from https://peterkovesi.com/projects/colourmaps/
+from PIL import Image
+colorfunctiondata = Image.open("diverging-linear_bjr_30-55_c53_n256.png")
+cfdata = np.asarray(colorfunctiondata)[-1]
+#print(cfdata.shape)
 
-totalflow = (flowNS**2+flowEW**2)**0.5
-totalflownorm = totalflow/np.max(totalflow)
-flowdir = np.arctan2(flowNS2,flowEW2)/(2*np.pi)
+def MyHSB2RGBfunc(hue, sat, bri):
+    #takes and outputs values between 0 and 1
+    hue = cfdata[int(round(511*hue))]/255.
+    hueav = np.sum(hue)/3
+    totalsat = np.array([hueav for i in range(3)])
+    return bri*(sat*hue + (1-sat)*totalsat)
 
-relief = np.gradient(mola, axis=0) + np.gradient(mola, axis=1)
-reliefnorm = relief/np.max(relief)
+# Let's try for flow direction too.
+colorfunctiondata2 = Image.open("cyclic-isoluminant.png")
+cfdata2 = np.asarray(colorfunctiondata2)[-3,10:-5]
+#print(cfdata2.shape)
 
-relief2 = np.gradient(mola+depth, axis=0) + np.gradient(mola+depth, axis=1)
-relief2norm = relief2/np.max(relief2)
+def MyHSB2RGBfunc2(hue, sat, bri):
+    #takes and outputs values between 0 and 1
+    hue = cfdata2[int(round((cfdata2.shape[0]-1)*hue))]/255.
+    hueav = np.sum(hue)/3
+    totalsat = np.array([hueav for i in range(3)])
+    return bri*(sat*hue + (1-sat)*totalsat)
 
-steepness = (np.gradient(mola, axis=0)**2 + np.gradient(mola, axis=1)**2)**0.5
-steepnessnorm = steepness/np.max(steepness)
+for latindex in range(4):
+    for lonindex in range(8):
+        print("writing: " + str(latindex) + "," + str(lonindex))
+        place = [latindex,lonindex]
+
+        rawdata = np.load(inputpath+"/array"+str(place[0])+str(place[1])+".npy")[2:-2,2:-2]
+
+        mola = rawdata[:,:,0]
+        depth = rawdata[:,:,2]
+        flowNS2 = np.gradient(mola+depth, axis=0)
+        flowNS = rawdata[:,:,3]
+        flowEW2 = np.gradient(mola+depth, axis=1)
+        flowEW = rawdata[:,:,4]
+        flownorm = rawdata[:,:,7]
+        
+        totalflow = (flowNS**2+flowEW**2)**0.5
+        totalflownorm = totalflow/np.max(totalflow)
+        flowdir = np.arctan2(flowNS2,flowEW2)/(2*np.pi)
+        
+        relief = np.gradient(mola, axis=0) + np.gradient(mola, axis=1)
+        reliefnorm = relief/np.max(relief)
+        
+        relief2 = np.gradient(mola+depth, axis=0) + np.gradient(mola+depth, axis=1)
+        relief2norm = relief2/np.max(relief2)
+
+        steepness = (np.gradient(mola, axis=0)**2 + np.gradient(mola, axis=1)**2)**0.5
+        steepnessnorm = steepness/np.max(steepness)
+
+        im.imsave("colors720/image"+str(latindex)+str(lonindex)+".png",
+                  np.array([[MyHSB2RGBfunc(0.9-0.7*np.tanh(0.01*depth[i,j]),
+                                           1-np.tanh(100*totalflownorm[i,j]),
+                                           0.5+0.4*np.tanh(np.tanh(10*reliefnorm[i,j])+np.tanh(100*totalflownorm[i,j])))*(1-np.tanh(colors*0.1*depth[i,j])) + 
+                             MyHSB2RGBfunc2(0.5+flowdir[i,j],
+                                            1-np.tanh(50*totalflownorm[i,j]),
+                                            0.5+0.4*np.tanh(np.tanh(10*reliefnorm[i,j])+np.tanh(100*totalflownorm[i,j])))*np.tanh(colors*0.1*depth[i,j])
+                             for j in range(flowdir.shape[0])] 
+                            for i in range(flowdir.shape[1])]))
+
+
+
+
+
+
+
+
 
 #im.imsave("test.png", flownorm*((0.5+0.5*np.sign(flowNS))*flowNS
 #          -(0.5-0.5*np.sign(flowNS))*flowNS))
@@ -87,19 +139,6 @@ steepnessnorm = steepness/np.max(steepness)
 #                                              0.5-0.4*np.tanh(10*reliefnorm[i,j])
 #                                          ) for i in range(flowdir.shape[0])] for j in range(flowdir.shape[1])]))
 
-#Time to home cook a HSB2RGB function.
-# from https://peterkovesi.com/projects/colourmaps/
-from PIL import Image
-colorfunctiondata = Image.open("diverging-linear_bjr_30-55_c53_n256.png")
-cfdata = np.asarray(colorfunctiondata)[-1]
-#print(cfdata.shape)
-
-def MyHSB2RGBfunc(hue, sat, bri):
-    #takes and outputs values between 0 and 1
-    hue = cfdata[int(round(511*hue))]/255.
-    hueav = np.sum(hue)/3
-    totalsat = np.array([hueav for i in range(3)])
-    return bri*(sat*hue + (1-sat)*totalsat)
 
 #print([[0.1*depthindex,round(511*np.tanh(0.002*0.1*depthindex))] for depthindex in range(1001)])
 
@@ -124,34 +163,13 @@ def MyHSB2RGBfunc(hue, sat, bri):
 #                                          ) for i in range(flowdir.shape[0])] for j in range(flowdir.shape[1])]))
 
 #This is a good one
-im.imsave("test.png",
-          np.array([[MyHSB2RGBfunc(0.9-0.7*np.tanh(0.01*depth[i,j]),
-                                   1-np.tanh(100*totalflownorm[i,j]),
-                                   0.5+0.4*np.tanh(np.tanh(10*reliefnorm[i,j])+np.tanh(100*totalflownorm[i,j]))
-                               ) 
-                     for i in range(flowdir.shape[0])] 
-                    for j in range(flowdir.shape[1])]))
-
-
-# Let's try for flow direction too.
-colorfunctiondata2 = Image.open("cyclic-isoluminant.png")
-cfdata2 = np.asarray(colorfunctiondata2)[-3,10:-5]
-#print(cfdata2.shape)
-
-def MyHSB2RGBfunc2(hue, sat, bri):
-    #takes and outputs values between 0 and 1
-    hue = cfdata2[int(round((cfdata2.shape[0]-1)*hue))]/255.
-    hueav = np.sum(hue)/3
-    totalsat = np.array([hueav for i in range(3)])
-    return bri*(sat*hue + (1-sat)*totalsat)
-
-
 #im.imsave("test.png",
 #          np.array([[MyHSB2RGBfunc(0.9-0.7*np.tanh(0.01*depth[i,j]),
 #                                   1-np.tanh(100*totalflownorm[i,j]),
-#                                   0.5+0.4*np.tanh(np.tanh(10*reliefnorm[i,j])+np.tanh(100*totalflownorm[i,j])))*(1-np.tanh(0.1*depth[i,j])) + 
-#                     MyHSB2RGBfunc2(0.5+flowdir[i,j],
-#                                    1-np.tanh(50*totalflownorm[i,j]),
-#                                    0.5+0.4*np.tanh(np.tanh(10*reliefnorm[i,j])+np.tanh(100*totalflownorm[i,j])))*np.tanh(0.1*depth[i,j])
+#                                   0.5+0.4*np.tanh(np.tanh(10*reliefnorm[i,j])+np.tanh(100*totalflownorm[i,j]))
+#                               ) 
 #                     for i in range(flowdir.shape[0])] 
 #                    for j in range(flowdir.shape[1])]))
+
+
+
